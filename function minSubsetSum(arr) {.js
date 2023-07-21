9afbae21ad62fd2af6b6fd00a2d4b348465b8b17
Shiyam4896 <133642510+Shiyam4896@users.noreply.github.com>
2023-07-21 09:43:47 +0530

    To find Minsum of an array
    The problem involves the logic to solve the min sum of the subset of an array .

---
+++ function minSubsetSum(arr) {.js
@@ -0,0 +1,16 @@
+function minSubsetSum(arr) {
+    let currentMin = arr[0];
+    let globalMin = arr[0];
+  
+    for (let i = 1; i < arr.length; i++) {
+      currentMin = Math.min(arr[i], currentMin + arr[i]);
+      globalMin = Math.min(globalMin, currentMin);
+    }
+  
+    return globalMin;
+  }
+  
+  const arr = [1, -2, 5, -3, -1];
+  const result = minSubsetSum(arr);
+  console.log("Minimum sum from subset:", result); // Output: -6
+  
