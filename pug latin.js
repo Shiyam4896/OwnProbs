4878c66d7c5972faceb63b3878894ad0f0b65282
Shiyam4896 <133642510+Shiyam4896@users.noreply.github.com>
2023-07-21 22:15:36 +0530

    Pig Latin problem
    The code involves solving the logic for converting the English words to "Pig Latin" .

---
+++ pug latin.js
@@ -0,0 +1,41 @@
+
+function toPigLatin(word) {
+    
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+  
+    
+    let firstVowelIndex = -1;
+  
+    
+    for (let i = 0; i < word.length; i++) {
+      if (vowels.includes(word[i].toLowerCase())) {
+        firstVowelIndex = i;
+        break;
+      }
+    }
+  
+    if (firstVowelIndex === 0) {
+      return word + 'way';
+    }
+    else if (firstVowelIndex > 0) {
+      const consonantCluster = word.slice(0, firstVowelIndex);
+      const restOfWord = word.slice(firstVowelIndex);
+      return restOfWord + consonantCluster + 'ay';
+    }
+    else {
+      return word;
+    }
+  }
+  
+  function sentenceToPigLatin(sentence) {
+    const words = sentence.split(' ');
+  
+    const pigLatinWords = words.map(word => toPigLatin(word));
+  
+    return pigLatinWords.join(' ');
+  }
+  
+  const sentence = "Hello, how are you doing today?";
+  const pigLatinSentence = sentenceToPigLatin(sentence);
+  console.log(pigLatinSentence); 
+  
